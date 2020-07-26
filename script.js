9aa8f4d998db16f15e3f8254235239a85a95cd56
Alex Ian Smith <taxihelms0@gmail.com>
2020-07-26 16:00:58 -0500

    change background on hover w/ js

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 const body = document.querySelector('body');
 const container = document.createElement('div.container');
-container.style.display = 'grid';
-container.style.gridTemplateColumns = 'repeat(16, 1fr)';
 
+// style container
+container.style.display = 'grid';
+container.style.gridTemplateColumns = 'repeat(16, 16px)';
+container.style.gridTemplateRows = 'repeat(16, 16px)'
+
+// change background
+function changeBackground(e) {
+  e.currentTarget.style.backgroundColor = 'red';
+}
+// create individual cells (16 rows x 16 cols)
 for(let i=0; i < 16; i++){
   for(let j=0; j < 16; j++){
-    const gridItem = document.createElement('div');
+    gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
     gridItem.id = `row-${i}-col-${j}`;
+    // styling
     gridItem.style.display = 'inline-block';
-    // gridItem.style.width = '16px';
     gridItem.style.height = '16px';
     gridItem.style.borderStyle = 'solid';
-    gridItem.style.borderWidth = '2px';
+    gridItem.style.borderWidth = '1px';
     container.appendChild(gridItem);
+    // mouseover
+    gridItem.addEventListener('mouseover', changeBackground);
   }
 }
-
-
-
 body.appendChild(container);
-
-/*
-border-style: solid;
-  border-width: 5px;
-*/
